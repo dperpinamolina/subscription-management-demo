1b6d2fcab752da6148ef64896fcda0fe6e255fe8
dperpina <perpinad@nordex-ag.com>
2024-03-21 14:00:14 +0100

    filter done

--- src/app/features/subscription-management/filter/filter.component.ts
+++ src/app/features/subscription-management/filter/filter.component.ts
@@ -38,9 +38,56 @@ export class FilterComponent {
 
   filtersForm = inject(FormBuilder).nonNullable.group({
     windfarms: [[]],
-    devices: [[]],
-    resolution: [[]],
-    signals: [[]],
+    devices: [{ value: [] as Array<string>, disabled: true }],
+    resolution: [{ value: '', disabled: true }],
+    signals: [{ value: [] as Array<string>, disabled: true }],
     fromTo: [{ start: new Date(2024, 2, 10), end: new Date(2024, 3, 10) }],
   });
+
+  constructor() {
+    this.validControlWindfarms();
+    this.validControlDevices();
+    this.validControlResolution();
+  }
+
+  validControlWindfarms() {
+    this.filtersForm.controls['windfarms'].valueChanges.subscribe(
+      (v: Array<string>) => {
+        if (v.length > 0) {
+          this.filtersForm.controls['devices'].enable();
+        } else {
+          this.filtersForm.controls['devices'].disable();
+        }
+        this.filtersForm.controls['devices'].reset();
+        this.filtersForm.controls['resolution'].reset();
+        this.filtersForm.controls['resolution'].disable();
+        this.filtersForm.controls['signals'].reset();
+        this.filtersForm.controls['signals'].disable();
+      }
+    );
+  }
+
+  validControlDevices() {
+    this.filtersForm.controls['devices'].valueChanges.subscribe((v: Array<string>) => {
+      if (v.length > 0) {
+        this.filtersForm.controls['resolution'].enable();
+      } else {
+        this.filtersForm.controls['resolution'].disable();
+      }
+      this.filtersForm.controls['resolution'].reset();
+      this.filtersForm.controls['signals'].reset();
+      this.filtersForm.controls['signals'].disable();
+    });
+  }
+
+  validControlResolution() {
+    this.filtersForm.controls['resolution'].valueChanges.subscribe((v: string) => {
+      if (v) {
+        this.filtersForm.controls['signals'].enable();
+      } else {
+        this.filtersForm.controls['signals'].disable();
+      }
+      this.filtersForm.controls['signals'].reset();
+    });
+  }
 }
